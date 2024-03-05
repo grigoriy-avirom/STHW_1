6449311347c9a95a71d4fdf1ce1abdc827a66b30
Grigoriy Avirom <88avirom@gmail.com>
2024-03-05 23:57:49 +0300

    Созданы классы-помощники и класс-менеджер, для инициализации помощников.

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import pytest
-from contacts import Contacts
+from model.contacts import Contacts
 from fixture.application import Application
 
 
@@ -12,9 +12,9 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
-    app.create_contact(Contacts(firstname="qwe", middlename="asd", lastname="zxc", nickname="qwerty", title="asdfgh", company="zxcvbn", address="dfjhgdfkjhgdjkfhg", home="123", mobile="456",
+    app.session.login(username="admin", password="secret")
+    app.contacts.create_contact(Contacts(firstname="qwe", middlename="asd", lastname="zxc", nickname="qwerty", title="asdfgh", company="zxcvbn", address="dfjhgdfkjhgdjkfhg", home="123", mobile="456",
                         work="789", fax="000", email="rty", email2="fgh", email3="fgh", homepage="iop", bday="1", bmonth="January", byear="2000", aday="1", amonth="January", ayear="2025"))
-    app.return_to_contacts_page()
-    app.logout()
+    app.contacts.return_to_contacts_page()
+    app.session.logout()
 
