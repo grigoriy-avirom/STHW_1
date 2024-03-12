4eb2cd51c9979db06692da2008384d434673c844
Grigoriy Avirom <88avirom@gmail.com>
2024-03-12 21:48:45 +0300

    Добавил test_modify_group

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,13 +2,13 @@
 from model.group import Group
 
 
-def test_add_group(app):
+def test_modify_group_name(app):
     app.session.login(username="admin", password="secret")
-    app.group.create(Group(name="name", header="header", footer="footer"))
+    app.group.modify_first_group(Group(name='New group'))
     app.session.logout()
 
 
-def test_add_empty_group(app):
+def test_modify_group_header(app):
     app.session.login(username="admin", password="secret")
-    app.group.create(Group(name="", header="", footer=""))
+    app.group.modify_first_group(Group(header='New header'))
     app.session.logout()
