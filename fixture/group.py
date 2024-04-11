9fa8412709adf39fa5613e1434b78831c82ac206
Grigoriy Avirom <88avirom@gmail.com>
2024-04-11 16:58:35 +0300

    Переделываем тесты: проверяем данные, загруженные из базы данных

--- fixture/group.py
+++ fixture/group.py
@@ -39,9 +39,22 @@ class GroupHelper:
         self.open_groups_page()
         self.group_cache = None
 
+    def delete_group_by_id(self, id):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_group_by_id(id)
+        # submit deletion
+        wd.find_element_by_name('delete').click()
+        self.open_groups_page()
+        self.group_cache = None
+
     def select_group_by_index(self, index):
         wd = self.app.wd
         wd.find_elements_by_name('selected[]')[index].click()
+
+    def select_group_by_id(self, id):
+        wd = self.app.wd
+        wd.find_element_by_css_selector('input[value="%s"]' % id).click()
 
     def fill_group_form(self, group):
         wd = self.app.wd
